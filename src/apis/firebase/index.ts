a573ea812f90aa528b68278d65d713aa6056d756
JuanDNJ <juandevnjv@gmail.com>
2023-05-11 19:55:31 +0200

    Borrando config firebase

--- src/apis/firebase/index.ts
+++ src/apis/firebase/index.ts
@@ -3,12 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
-    apiKey: "AIzaSyC6tTX9dnIFU7BK52mRTrXHLUEf8bUSZlc",
-    authDomain: "admin-app-b591e.firebaseapp.com",
-    projectId: "admin-app-b591e",
-    storageBucket: "admin-app-b591e.appspot.com",
-    messagingSenderId: "217918207735",
-    appId: "1:217918207735:web:f1952ae59193ad365185f2"
+  
 };
 
 const app = initializeApp(firebaseConfig);
